4121ba00e491f114f7bf610362bda4cfa7873bd2
Tiago Balbino <tiago1haise@gmail.com>
2024-08-26 14:59:12 -0300

    feature: create structure of project.

---
+++ src/main/java/tiago/balbino/admincatalogo/AdminCatalogoApplication.java
@@ -0,0 +1,13 @@
+package tiago.balbino.admincatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCatalogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminCatalogoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tiago/balbino/admincatalogo/AdminCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package tiago.balbino.admincatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminCatalogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
